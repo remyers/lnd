fa612fa671ffb22686701f624b14adbef9ebf629
Richard Myers <8568892+remyers@users.noreply.github.com>
2020-10-13 09:36:12 +0200

    build proto files for mobile

--- go.mod
+++ go.mod
@@ -65,6 +65,7 @@ require (
 	github.com/xiang90/probing v0.0.0-20190116061207-43a291ad63a2 // indirect
 	go.uber.org/zap v1.14.1 // indirect
 	golang.org/x/crypto v0.0.0-20200709230013-948cd5f35899
+	golang.org/x/mobile v0.0.0-20200801112145-973feb4309de // indirect
 	golang.org/x/net v0.0.0-20191002035440-2ec189313ef0
 	golang.org/x/time v0.0.0-20180412165947-fbb02b2291d2
 	google.golang.org/grpc v1.24.0
--- lnrpc/gen_protos.sh
+++ lnrpc/gen_protos.sh
@@ -12,17 +12,20 @@ for file in $PROTOS; do
 
   # Generate the protos.
   protoc -I/usr/local/include -I. \
+    --java_out=. \
     --go_out=plugins=grpc,paths=source_relative:. \
     "${file}"
 
   # Generate the REST reverse proxy.
   protoc -I/usr/local/include -I. \
+    --java_out=. \
     --grpc-gateway_out=logtostderr=true,paths=source_relative,grpc_api_configuration=rest-annotations.yaml:. \
     "${file}"
 
 
   # Finally, generate the swagger file which describes the REST API in detail.
   protoc -I/usr/local/include -I. \
+    --java_out=. \
     --swagger_out=logtostderr=true,grpc_api_configuration=rest-annotations.yaml:. \
     "${file}"
 done
